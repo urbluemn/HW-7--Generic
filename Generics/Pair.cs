e0351d5404f52de9cf500bd0da5f5f2f6bc56e42
urbluemn <nyaroshko1404@gmail.com>
2022-11-08 02:43:48 +0300

    done i guess

---
+++ Generics/Pair.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Pair<S, T>
+    {
+        public S Name { get; private set; }
+        public T Id { get; private set; }
+
+        public Pair(S name, T id)
+            {
+            Name = name;
+            Id = id;
+            }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using Generics;
+
+var pair = new Pair<string, int>("Nick", 22);
+
+Console.WriteLine(pair.Name);
+Console.WriteLine(pair.Id);
